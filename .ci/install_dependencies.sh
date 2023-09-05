90afe629a37acee0f42d95fe1c307f02a52a8f1c
Martin Hunt <martinghunt@users.noreply.github.com>
2023-09-05 16:32:15 +0100

    Manual mafft install

--- .ci/install_dependencies.sh
+++ .ci/install_dependencies.sh
@@ -21,10 +21,25 @@ if [ ! -d $install_root ]; then
 fi
 cd $install_root
 
+
+# usher gets x86_64 faToVcf. So get precompiled mafft also for x86. Could
+# change this later if needed by compiling mafft from source. Note - usher
+# also installs mafft using at-get. We stop it from doing so because
+# bash process subsitution doesn't work in that version,
+# which is how ushonium python wrapper code runs it
+wget https://mafft.cbrc.jp/alignment/software/mafft-7.520-linux.tgz
+tar xf mafft-7.520-linux.tgz
+mv mafft-linux64/mafft.bat mafft-linux64/mafft
+rm mafft-7.520-linux.tgz
+
+
 git clone https://github.com/yatisht/usher.git
 cd usher
 # There's no sudo in the singularity build
 sed -i 's/sudo -E //' install/installUbuntu.sh
+# Prevent mafft install because bash process subsitution doesn't work in
+# the version from apt get, which is how ushonium python wrapper code runs it
+sed -i 's/mafft//' install/installUbuntu.sh
 # use 1 thread to compile. On my 8gb build machine, using all 4 cores dies
 # because it uses all the ram
 sed -i 's/make -j$(nproc)/make -j2/' install/installUbuntu.sh
